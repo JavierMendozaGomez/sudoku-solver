af289b56c7b1c6b965f8e00eae1f5eaf0e7bcf26
Javier Mendoza Gomez <javierenriquemendozagomez@gmail.com>
2017-03-26 20:57:10 +0200

    adsa
    qweqe

--- index.js
+++ index.js
@@ -1,20 +1,127 @@
-var matriz = [[{seccion:1},{seccion:1},{seccion:1},{seccion:2},{seccion:2},{seccion:2},{seccion:3},{seccion:3},{seccion:3}]]
+var MatrizPerfecta =[
+  [{fila:0,col:0, valor: -1 ,seccion:0 },{fila:0,col:1, valor: -1 ,seccion:0 },{fila:0,col:2, valor: -1 ,seccion:0 },{fila:0,col:3, valor: -1 ,seccion:1 },{fila:0,col:4, valor: -1 ,seccion:1 },{fila:0,col:5, valor: -1 ,seccion:1 },{fila:0,col:6, valor: -1 ,seccion:2 },{fila:0,col:7, valor: -1 ,seccion:2 },{fila:0,col:9, valor: -1 ,seccion:2 }],
+  [{fila:1,col:0, valor: -1 ,seccion:0 },{fila:1,col:1, valor: -1 ,seccion:0 },{fila:1,col:2, valor: -1 ,seccion:0 },{fila:1,col:3, valor: -1 ,seccion:1 },{fila:1,col:4, valor: -1 ,seccion:1 },{fila:1,col:5, valor: -1 ,seccion:1 },{fila:1,col:6, valor: -1 ,seccion:2 },{fila:1,col:7, valor: -1 ,seccion:2 },{fila:1,col:9, valor: -1 ,seccion:2 }],
+  [{fila:2,col:0, valor: -1 ,seccion:0 },{fila:2,col:1, valor: -1 ,seccion:0 },{fila:2,col:2, valor: -1 ,seccion:0 },{fila:2,col:3, valor: -1 ,seccion:1 },{fila:2,col:4, valor: -1 ,seccion:1 },{fila:2,col:5, valor: -1 ,seccion:1 },{fila:2,col:6, valor: -1 ,seccion:2 },{fila:2,col:7, valor: -1 ,seccion:2 },{fila:2,col:9, valor: -1 ,seccion:2 }],
+  [{fila:3,col:0, valor: -1 ,seccion:3 },{fila:3,col:1, valor: -1 ,seccion:3 },{fila:3,col:2, valor: -1 ,seccion:3 },{fila:3,col:3, valor: -1 ,seccion:4 },{fila:3,col:4, valor: -1 ,seccion:4 },{fila:3,col:5, valor: -1 ,seccion:4 },{fila:3,col:6, valor: -1 ,seccion:5 },{fila:3,col:7, valor: -1 ,seccion:5 },{fila:3,col:9, valor: -1 ,seccion:5 }],
+  [{fila:4,col:0, valor: -1 ,seccion:3 },{fila:4,col:1, valor: -1 ,seccion:3 },{fila:4,col:2, valor: -1 ,seccion:3 },{fila:4,col:3, valor: -1 ,seccion:4 },{fila:4,col:4, valor: -1 ,seccion:4 },{fila:4,col:5, valor: -1 ,seccion:4 },{fila:4,col:6, valor: -1 ,seccion:5 },{fila:4,col:7, valor: -1 ,seccion:5 },{fila:4,col:9, valor: -1 ,seccion:5 }],
+  [{fila:5,col:0, valor: -1 ,seccion:3 },{fila:5,col:1, valor: -1 ,seccion:3 },{fila:5,col:2, valor: -1 ,seccion:3 },{fila:5,col:3, valor: -1 ,seccion:4 },{fila:5,col:4, valor: -1 ,seccion:4 },{fila:5,col:5, valor: -1 ,seccion:4 },{fila:5,col:6, valor: -1 ,seccion:5 },{fila:5,col:7, valor: -1 ,seccion:5 },{fila:5,col:9, valor: -1 ,seccion:5 }],
+  [{fila:6,col:0, valor: -1 ,seccion:6 },{fila:6,col:1, valor: -1 ,seccion:6 },{fila:6,col:2, valor: -1 ,seccion:6 },{fila:6,col:3, valor: -1 ,seccion:7 },{fila:6,col:4, valor: -1 ,seccion:7 },{fila:6,col:5, valor: -1 ,seccion:7 },{fila:6,col:6, valor: -1 ,seccion:8 },{fila:6,col:7, valor: -1 ,seccion:8 },{fila:6,col:9, valor: -1 ,seccion:8 }],
+  [{fila:7,col:0, valor: -1 ,seccion:6 },{fila:7,col:1, valor: -1 ,seccion:6 },{fila:7,col:2, valor: -1 ,seccion:6 },{fila:7,col:3, valor: -1 ,seccion:7 },{fila:7,col:4, valor: -1 ,seccion:7 },{fila:7,col:5, valor: -1 ,seccion:7 },{fila:7,col:6, valor: -1 ,seccion:8 },{fila:7,col:7, valor: -1 ,seccion:8 },{fila:7,col:9, valor: -1 ,seccion:8 }],
+  [{fila:8,col:0, valor: -1 ,seccion:6 },{fila:8,col:1, valor: -1 ,seccion:6 },{fila:8,col:2, valor: -1 ,seccion:6 },{fila:8,col:3, valor: -1 ,seccion:7 },{fila:8,col:4, valor: -1 ,seccion:7 },{fila:8,col:5, valor: -1 ,seccion:7 },{fila:8,col:6, valor: -1 ,seccion:8 },{fila:8,col:7, valor: -1 ,seccion:8 },{fila:8,col:9, valor: -1 ,seccion:8 }]]
 
-function filaDeSeccionLlena( casilla, secciones){
-  return (secciones[casilla.seccion].fila[casilla.fila].numColocadas == 3)
+var valoresSinRellenar = [
+               [-1,-1,-1,   -1,-1,-1,  -1,-1,-1 ],
+               [-1,-1,-1,   -1,-1,-1,  -1,-1,-1 ],
+               [-1,-1,-1,   -1,-1,-1,  -1,-1,-1 ],
+
+               [-1,-1,-1,   -1,-1,-1,  -1,-1,-1 ],
+               [-1,-1,-1,   -1,-1,-1,  -1,-1,-1 ],
+               [-1,-1,-1,   -1,-1,-1,  -1,-1,-1 ],
+
+               [-1,-1,-1,   -1,-1,-1,  -1,-1,-1 ],
+               [-1,-1,-1,   -1,-1,-1,  -1,-1,-1 ],
+               [-1,-1,-1,   -1,-1,-1,  -1,-1,-1 ],
+             ];
+var valoresRellenos = [
+                            [4,1,-1,   -1,-1,-1,  -1,6,-1 ],
+                            [-1,3,-1,  -1,-1,1,  -1,7,-1 ],
+                            [8,-1,2,   9,-1,5     1,-1,4 ],
+
+                            [-1,2,5,   -1,-1,-1,  -1,1,8 ],
+                            [9,-1,-1,   8,-1,2,  -1,-1,-1 ],
+                            [6,4,8,   -1,-1,-1,  -1,9,-1 ],
+
+                            [-1,-1,-1,   -1,-1,-1,  5,4,-1 ],
+                            [-1,-1,-1,   -1,-1,8,  -1,-1,-1 ],
+                            [-1,-1,-1,   -1,5,4,  6,8,1 ],
+                          ]
+
+function seccionSuperiorLlena(casilla,secciones){
+  return secciones[casilla.seccion].numSuperiorColocadas == 3
+}
+function seccionIntermediaLlena(casilla,secciones){
+  return secciones[casilla.seccion].numIntermediasColocadas == 3
+}
+function seccionInferiorLlena(casilla,secciones){
+  return secciones[casilla.seccion].numInferioresColocadas == 3
+}
+function seccionEsteLlena(casilla,secciones){
+  return secciones[casilla.seccion].numEsteColocadas == 3
+}
+function seccionCentroLlena(casilla,secciones){
+  return secciones[casilla.seccion].numCentroColocadas == 3
+}
+function seccionOesteLlena(casilla,secciones){
+  return secciones[casilla.seccion].numOesteColocadas == 3
 }
 
-function colDeSeccionLLena(casilla, secciones){
-  return(secciones[casilla.seccion].col[casilla.col].numColocadas == 9)
+function colEsteContieneValor(casilla, cols){
+  var colEste;
+  if(casilla.seccion == 0 || casilla.seccion == 1 || casilla.seccion == 2)
+      colEste = 0;
+  else if(casilla.seccion == 3 || casilla.seccion == 4 || casilla.seccion == 5)
+      colEste = 3
+  else
+      colEste = 6
+  return cols[colEste].has(casilla.valor)
 }
 
-function filaContieneValor(casilla,filas){
-    return filas[casilla.fila].has(casilla.valor)
-}
-function colContieneValor(casilla,cols){
-    return cols[casilla.col].has(casilla.valor)
+function colCentroContieneValor(casilla, cols){
+  var colCentro;
+  if(casilla.seccion == 0 || casilla.seccion == 1 || casilla.seccion == 2)
+      colCentro = 1;
+  else if(casilla.seccion == 3 || casilla.seccion == 4 || casilla.seccion == 5)
+      colCentro = 4
+  else
+      colCentro = 7
+  return cols[colCentro].has(casilla.valor)
 }
 
+function colOesteContieneValor(casilla, cols){
+  var colOeste;
+  if(casilla.seccion == 0 || casilla.seccion == 1 || casilla.seccion == 2)
+      colOeste = 2;
+  else if(casilla.seccion == 3 || casilla.seccion == 4 || casilla.seccion == 5)
+      colOeste = 5
+  else
+      colOeste = 8
+  return cols[colOeste].has(casilla.valor)
+}
+
+
+function filaSuperiorContieneValor(casilla,filas){
+  var filaSuperior;
+  if(casilla.seccion == 0 || casilla.seccion == 1 || casilla.seccion == 2)
+      filaSuperior = 0;
+  else if(casilla.seccion == 3 || casilla.seccion == 4 || casilla.seccion == 5)
+      filaSuperior = 3
+  else
+      filaSuperior = 6
+  return filas[filaSuperior].has(casilla.valor)
+}
+
+function filaIntermediaContieneValor(casilla,filas){
+  var filaIntermedia;
+  if(casilla.seccion == 0 || casilla.seccion == 1 || casilla.seccion == 2)
+      filaIntermedia = 1;
+  else if(casilla.seccion == 3 || casilla.seccion == 4 || casilla.seccion == 5)
+      filaIntermedia = 4
+  else
+      filaIntermedia = 7
+  return filas[filaIntermedia].has(casilla.valor)
+}
+
+
+function filaInferiorContieneValor(casilla,filas){
+  var filaInferior;
+  if(casilla.seccion == 0 || casilla.seccion == 1 || casilla.seccion == 2)
+      filaInferior = 2;
+  else if(casilla.seccion == 3 || casilla.seccion == 4 || casilla.seccion == 5)
+      filaInferior = 5
+  else
+      filaInferior = 8
+  return filas[filaInferior].has(casilla.valor)
+}
 
 function esFilaSuperior(casilla){
   return (casilla.fila == 0 || casilla.fila == 3|| casila.fila == 6 )
@@ -41,8 +148,20 @@ function esColOeste(casilla){
 }
 
 function colocaCasilla(casilla, secciones, filas, cols, casillasFijas){
-    secciones[casilla.seccion].fila[casilla.fila].numColocadas++
-    secciones[casilla.seccion].col[casilla.col].numColocadas++;
+    if(esFilaSuperior(casilla))
+      secciones[casilla.seccion].numSuperiorColocadas++
+    else if(esFilaIntermedia(casilla))
+      secciones[casilla.seccion].numIntermediasColocadas++
+    else
+      secciones[casilla.seccion].numInferioresColocadas++
+    if(esColEste(casilla))
+      secciones[casilla.seccion].numEsteColocadas++
+    else if(esColOeste(casilla))
+      secciones[casilla.seccion].numOesteColocadas++
+    else
+      secciones[casilla.seccion].numEsteColocadas++
+
+    secciones[casilla.seccion].casillasColocadas.set(casilla.valor,true)
     casillasFijas[casilla.fila][casilla.col] = true;
     cols[casilla.col].set(casilla.valor, true)
     filas[casilla.fila].set(casilla.valor, true)
@@ -51,19 +170,21 @@ function colocaCasilla(casilla, secciones, filas, cols, casillasFijas){
 function esCasillaFija(casilla, casillasFijas){
     return casillasFijas[casilla.fila][casilla.col]
 }
-
+function cargaMatriz(mat
 function cargaCasillasFijas(matriz,secciones,filas,cols, callback){
   var casillasFijas = new Array(9)
   for(var i = 0; i < 9; i++)
       casillasFijas[i] = new Array(9)
 
-  for(var i = 0; i < 9; i++)
+  for(var i = 0; i < 9; i++){
     for(var j = 0; j < 9; j++){
         if(matriz[i][j].valor != -1 ){
           var casilla = {i:i,j:j, valor: matriz[i][j].valor}
           colocaCasilla(casilla, secciones, filas, cols, casillasFijas)
         }
       }
+    }
+    console.log(secciones)
 }
 
 function inicializa(callback){
@@ -78,11 +199,14 @@ function inicializa(callback){
   }
   secciones = new Array(9);
   for(var i = 0; i < secciones.length; i++){
-      secciones[i] = {fila: new Array(9), col: new Array(9)}
-      for(var j = 0; j < 9; j++){
-          secciones[i].fila[j] = {numColocadas: 0}
-          secciones[i].col[j] = {numColocadas: 0}
-      }
+      secciones[i] = {}
+      secciones[i].numSuperiorColocadas = 0
+      secciones[i].numIntermediasColocadas = 0
+      secciones[i].numInferioresColocadas = 0
+      secciones[i].numEsteColocadas = 0
+      secciones[i].numCentroColocadas = 0
+      secciones[i].numOesteColocadas = 0
+      secciones[i].casillasColocadas = new Map()
   }
   callback(null,{ filas: filas, cols: cols, secciones:secciones})
 }
@@ -93,7 +217,17 @@ inicializa(function(err, result){
           secciones = result.secciones
           filas = result.filas
           cols = result.cols
-          console.log(secciones)
           }
     }
 )
+/*var linea = "["
+var i, j
+for(i = 0; i < 9; i++){
+    for( j = 0; j < 8;j++){
+    linea+="{i:"+i+",j:"+j+", valor: -1 ,seccion: },"}
+    linea+="{i:"+i+",j:"+(j+1)+", valor: -1 ,seccion: }"
+    linea+="]"
+    console.log(linea)
+    linea="["}
+*/
+console.log(MatrizPerfecta)
